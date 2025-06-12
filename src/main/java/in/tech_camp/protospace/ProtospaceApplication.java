708f62f88c2ca9816f089d46471c85a2fdbc308c
yamadasynapse <tyamada@synapse.jp>
2025-06-12 16:11:01 +0900

    Reapply "main"
    This reverts commit db814f296a52fd851defca20faa21de41469598c.

---
+++ src/main/java/in/tech_camp/protospace/ProtospaceApplication.java
@@ -0,0 +1,13 @@
+package in.tech_camp.protospace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtospaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtospaceApplication.class, args);
+	}
+
+}
